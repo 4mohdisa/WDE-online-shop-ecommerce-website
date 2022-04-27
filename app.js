a26c598e601f5a5382e67b392f78140559aa8bd9
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2022-04-27 15:20:04 +0530

    complete WDE website

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const checkAuthStatusMiddleware = require('./middlewares/check-auth');
 const protectRoutesMiddleware = require('./middlewares/protect-routes');
 const cartMiddleware = require('./middlewares/cart');
 const updateCartPricesMiddleware = require('./middlewares/update-cart-prices');
-const notFoundMiddleware = require('./middlewares/not-found');
 const authRoutes = require('./routes/auth.routes');
 const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
@@ -45,10 +44,9 @@ app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
 app.use('/cart', cartRoutes);
-app.use('/orders', protectRoutesMiddleware, ordersRoutes);
-app.use('/admin', protectRoutesMiddleware, adminRoutes);
-
-app.use(notFoundMiddleware);
+app.use(protectRoutesMiddleware);
+app.use('/orders', ordersRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(errorHandlerMiddleware);
 
--- public/scripts/cart-management.js
+++ public/scripts/cart-management.js
@@ -1,5 +1,5 @@
 const addToCartButtonElement = document.querySelector('#product-details button');
-const cartBadgeElements = document.querySelectorAll('.nav-items .badge');
+const cartBadgeElement = document.querySelector('.nav-items .badge');
 
 async function addToCart() {
   const productId = addToCartButtonElement.dataset.productid;
@@ -32,9 +32,7 @@ async function addToCart() {
 
   const newTotalQuantity = responseData.newTotalItems;
 
-  for (const cartBadgeElement of cartBadgeElements) {
-    cartBadgeElement.textContent = newTotalQuantity;
-  }
+  cartBadgeElement.textContent = newTotalQuantity;
 }
 
 addToCartButtonElement.addEventListener('click', addToCart);
